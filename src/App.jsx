03e0f05cfd00648c7daa2f72557980e6c2ef094d
akliyachir <akli.yachir@gmail.com>
2023-02-18 00:51:50 -0300

    starting react app with vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import {
+	createBrowserRouter,
+	createRoutesFromElements,
+	Route,
+	RouterProvider,
+} from 'react-router-dom';
+import RootLayout from './page/RootLayout/RootLayout';
+
+const router = createBrowserRouter(
+	createRoutesFromElements(<Route path='/' element={<RootLayout />}></Route>)
+);
+
 function App() {
-	return <div className='App'></div>;
+	return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
