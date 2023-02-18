07797c0c0907d233c36661448ef67472d0e2ffd7
akliyachir <akli.yachir@gmail.com>
2023-02-17 21:08:10 -0300

    personal portfolio build, initialising nextjs app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,12 +7,11 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang='en'>
-			{/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
 			<head />
-			<body>{children}</body>
+			<body>
+				<main>{children}</main>
+				<footer>hola</footer>
+			</body>
 		</html>
 	);
 }
