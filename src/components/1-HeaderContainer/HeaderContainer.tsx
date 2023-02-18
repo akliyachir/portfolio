fa96538166fff19b1ac7dbe4cc080cb3d141ef02
akliyachir <akli.yachir@gmail.com>
2023-02-17 23:39:29 -0300

    header

--- src/components/1-HeaderContainer/HeaderContainer.tsx
+++ src/components/1-HeaderContainer/HeaderContainer.tsx
@@ -1,5 +1,13 @@
 import './HeaderContainer.css';
+import ThemeSwitch from '../2-ThemeSwitch/ThemeSwitch';
 
 export default function HeaderContainer() {
-	return <div className='HeaderContainer'>HeaderContainer</div>;
+	return (
+		<div className='HeaderContainer'>
+			<div>
+				<ThemeSwitch />
+			</div>
+			HeaderContainer
+		</div>
+	);
 }
---
+++ src/components/2-ThemeSwitch/ThemeSwitch.tsx
@@ -0,0 +1,12 @@
+import { BsSun } from 'react-icons/bs';
+import { GiMoonOrbit } from 'react-icons/gi';
+import './ThemeSwitch.css';
+export default function ThemeSwitch() {
+	return (
+		<div className='ThemeSwitch'>
+			<div className='switchContainer'>
+				<BsSun />
+			</div>
+		</div>
+	);
+}
