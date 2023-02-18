4ef38abd7f804686d148e41aec265731ec5be07a
akliyachir <akli.yachir@gmail.com>
2023-02-18 00:13:17 -0300

    header

--- src/components/1-HeaderContainer/HeaderContainer.tsx
+++ src/components/1-HeaderContainer/HeaderContainer.tsx
@@ -1,7 +1,6 @@
 'use client';
 import './HeaderContainer.css';
 import ThemeSwitch from '../2-ThemeSwitch/ThemeSwitch';
-import useState from 'react';
 
 export default function HeaderContainer() {
 	return (
