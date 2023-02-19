08e2b363349c93d97009b2a1bf21c2920ea2a259
akliyachir <akli.yachir@gmail.com>
2023-02-19 02:52:24 -0300

    project section build, and responsiveness

--- src/ListsAndFixedContent/projectsContent.js
+++ src/ListsAndFixedContent/projectsContent.js
@@ -1,10 +1,77 @@
 const projectsContent = [
 	{
 		id: 1,
-		project_name: 'articles publishing platform',
+		project_name: 'Articles Publishing Platform',
 		project_technologies: ['react...'],
-		project_description: 'a platform to publish articles',
+		project_description:
+			'a platform to publish articles, with user registration, authentification and authorizations. Password encryption, token and local storage. Foreign keys, and crud operations',
 		project_image_preview: '/projectsImages/1-articles-publishing-platform.png',
+		project_tech: {
+			backend: [
+				{
+					tech_id: 1,
+					tech_name: 'express',
+				},
+				{
+					tech_id: 2,
+					tech_name: 'mongoose',
+				},
+				{
+					tech_id: 3,
+					tech_name: 'jsonwebtoken',
+				},
+				{
+					tech_id: 4,
+					tech_name: 'bcrypt',
+				},
+				{
+					tech_id: 5,
+					tech_name: 'dotenv',
+				},
+				{
+					tech_id: 6,
+					tech_name: 'cors',
+				},
+				{
+					tech_id: 7,
+					tech_name: 'validator',
+				},
+			],
+			frontend: [
+				{
+					tech_id: 1,
+					tech_name: 'vite',
+				},
+				{
+					tech_id: 2,
+					tech_name: 'react 18',
+				},
+				{
+					tech_id: 3,
+					tech_name: 'react router dom 6.8',
+				},
+				{
+					tech_id: 4,
+					tech_name: 'tiptap',
+				},
+				{
+					tech_id: 5,
+					tech_name: 'html-react-parser',
+				},
+				{
+					tech_id: 6,
+					tech_name: 'local storage',
+				},
+				{
+					tech_id: 7,
+					tech_name: 'hooks',
+				},
+				{
+					tech_id: 8,
+					tech_name: 'vanilla css with grid & flexbox',
+				},
+			],
+		},
 	},
 ];
 
