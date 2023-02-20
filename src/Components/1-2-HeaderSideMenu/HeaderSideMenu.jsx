c4a59b6b2a14c003b72ae8c604b84cc2e891c493
akliyachir <akli.yachir@gmail.com>
2023-02-20 20:35:23 -0300

    menu bar interactivity

---
+++ src/Components/1-2-HeaderSideMenu/HeaderSideMenu.jsx
@@ -0,0 +1,5 @@
+import "./HeaderSideMenu.css";
+
+export default function HeaderSideMenu() {
+  return <div className="HeaderSideMenu">HeaderSideMenu</div>;
+}
--- src/Components/1-HeaderContainer/HeaderContainer.jsx
+++ src/Components/1-HeaderContainer/HeaderContainer.jsx
@@ -6,6 +6,7 @@ import HeaderMenuItem from "../3-HeaderMenuItem/HeaderMenuItem";
 import { WindowWidthContext } from "../../contexts/WindowWidth";
 import headerMenuItemsList from "../../ListsAndFixedContent/headerMenuItemsList";
 import MenuToggleHambourger from "../1-1-MenuToggleHambourger/MenuToggleHambourger";
+import HeaderSideMenu from "../1-2-HeaderSideMenu/HeaderSideMenu";
 
 export default function HeaderContainer() {
   const { isLightOn, setisLightOn } = useContext(ThemeSwitchContext);
@@ -13,6 +14,7 @@ export default function HeaderContainer() {
   console.log(WindowWidthValue);
   return (
     <div className="HeaderContainer">
+      <HeaderSideMenu />
       <LightSwitch />
       <div className="menuItemsExpanded">
         {headerMenuItemsList.map((item) => {
