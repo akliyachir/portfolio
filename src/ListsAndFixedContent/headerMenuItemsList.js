f5e32187b3a81f64970f66aa36d6cf9d5e412435
akliyachir <akli.yachir@gmail.com>
2023-02-20 19:18:19 -0300

    menu bar interactivity

--- src/ListsAndFixedContent/headerMenuItemsList.js
+++ src/ListsAndFixedContent/headerMenuItemsList.js
@@ -1,8 +1,8 @@
 const headerMenuItemsList = [
   { MenuItemId: 1, MenuItemName: "About me", MenuItemLink: "about_me" },
-  { MenuItemId: 2, MenuItemName: "Project", MenuItemLink: "project" },
-  { MenuItemId: 3, MenuItemName: "Curriculum", MenuItemLink: "curriculum" },
-  { MenuItemId: 4, MenuItemName: "Skills", MenuItemLink: "skills" },
+  { MenuItemId: 2, MenuItemName: "Projects", MenuItemLink: "projects" },
+  { MenuItemId: 3, MenuItemName: "Skills", MenuItemLink: "skills" },
+  { MenuItemId: 4, MenuItemName: "Curriculum", MenuItemLink: "curriculum" },
   { MenuItemId: 5, MenuItemName: "Contact", MenuItemLink: "contact" },
 ];
 
