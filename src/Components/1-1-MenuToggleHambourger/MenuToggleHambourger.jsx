42a8c7d04db3a82e9b7a4988581d2d65e28fcdfa
akliyachir <akli.yachir@gmail.com>
2023-02-20 19:45:28 -0300

    menu bar interactivity

---
+++ src/Components/1-1-MenuToggleHambourger/MenuToggleHambourger.jsx
@@ -0,0 +1,11 @@
+import "./MenuToggleHambourger.css";
+
+export default function MenuToggleHambourger() {
+  return (
+    <div className="MenuToggleHambourger">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  );
+}
--- src/Components/1-HeaderContainer/HeaderContainer.jsx
+++ src/Components/1-HeaderContainer/HeaderContainer.jsx
@@ -1,30 +1,32 @@
-import './HeaderContainer.css';
-import LightSwitch from '../2-LightSwitch/LightSwitch';
-import { useContext } from 'react';
-import { ThemeSwitchContext } from '../../contexts/ThemeSwitchContext';
-import HeaderMenuItem from '../3-HeaderMenuItem/HeaderMenuItem';
-import { WindowWidthContext } from '../../contexts/WindowWidth';
-import headerMenuItemsList from '../../ListsAndFixedContent/headerMenuItemsList';
+import "./HeaderContainer.css";
+import LightSwitch from "../2-LightSwitch/LightSwitch";
+import { useContext } from "react";
+import { ThemeSwitchContext } from "../../contexts/ThemeSwitchContext";
+import HeaderMenuItem from "../3-HeaderMenuItem/HeaderMenuItem";
+import { WindowWidthContext } from "../../contexts/WindowWidth";
+import headerMenuItemsList from "../../ListsAndFixedContent/headerMenuItemsList";
+import MenuToggleHambourger from "../1-1-MenuToggleHambourger/MenuToggleHambourger";
 
 export default function HeaderContainer() {
-	const { isLightOn, setisLightOn } = useContext(ThemeSwitchContext);
-	const WindowWidthValue = useContext(WindowWidthContext);
-	return (
-		<div className='HeaderContainer'>
-			<div></div>
-			<div className='menuItemsExpanded'>
-				{headerMenuItemsList.map((item) => {
-					const { MenuItemId, MenuItemName, MenuItemLink } = item;
-					return (
-						<HeaderMenuItem
-							key={MenuItemId}
-							name={MenuItemName}
-							link={MenuItemLink}
-						/>
-					);
-				})}
-			</div>
-			<LightSwitch />
-		</div>
-	);
+  const { isLightOn, setisLightOn } = useContext(ThemeSwitchContext);
+  const WindowWidthValue = useContext(WindowWidthContext);
+  console.log(WindowWidthValue);
+  return (
+    <div className="HeaderContainer">
+      <LightSwitch />
+      <div className="menuItemsExpanded">
+        {headerMenuItemsList.map((item) => {
+          const { MenuItemId, MenuItemName, MenuItemLink } = item;
+          return (
+            <HeaderMenuItem
+              key={MenuItemId}
+              name={MenuItemName}
+              link={MenuItemLink}
+            />
+          );
+        })}
+      </div>
+      <MenuToggleHambourger />
+    </div>
+  );
 }
