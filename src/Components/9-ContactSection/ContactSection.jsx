1f1064c0cb2219d2c59d961797c92d98d0a6bd81
akliyachir <akli.yachir@gmail.com>
2023-02-21 16:51:45 -0300

    Projects section

--- src/Components/9-ContactSection/ContactSection.jsx
+++ src/Components/9-ContactSection/ContactSection.jsx
@@ -2,14 +2,19 @@ import './ContactSection.css';
 import ContactInformations from '../../ListsAndFixedContent/ContactInformations';
 
 export default function ContactSection() {
-	const { contact_id, contact_full_name } = ContactInformations;
+	const { contact_id, contact_full_name, contact_phone_number } =
+		ContactInformations;
 	return (
 		<div className='ContactSection' id='contact'>
 			<div className='contactSectionTitle'>Contact</div>
 			<div key={contact_id} className='ContactSectionContent'>
-				<div>
-					<span className='ContactElementName'>Name |</span>
+				<div className='contactElementContainer'>
+					<span className='ContactElementName'>Name :</span>
 					<span>{contact_full_name} </span>
+				</div>
+				<div className='contactElementContainer'>
+					<span className='ContactElementName'>Phone :</span>
+					<span className='ContactElementInfo'>{contact_phone_number} </span>
 				</div>
 			</div>
 		</div>
--- src/ListsAndFixedContent/ContactInformations.js
+++ src/ListsAndFixedContent/ContactInformations.js
@@ -1,6 +1,7 @@
 const ContactInformations = {
 	contact_id: 1,
 	contact_full_name: 'Akli Yachir',
+	contact_phone_number: '(+52) 9 11 22 66 99 84',
 };
 
 export default ContactInformations;
