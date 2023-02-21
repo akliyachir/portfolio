de962e39680d5510f8ad86ec3b980758d904c881
akliyachir <akli.yachir@gmail.com>
2023-02-21 14:31:10 -0300

    skills section

--- src/ListsAndFixedContent/skillsItems.jsx
+++ src/ListsAndFixedContent/skillsItems.jsx
@@ -1,5 +1,11 @@
-import {} from 'react-icons/go';
-import { RiReactjsFill } from 'react-icons/ri';
+import { FaJs, FaCss3 } from 'react-icons/fa';
+import {} from 'react-icons/all';
+import {
+	RiReactjsFill,
+	RiNpmjsFill,
+	RiGithubFill,
+	RiHtml5Fill,
+} from 'react-icons/ri';
 
 const skillsItems = [
 	{
@@ -9,8 +15,44 @@ const skillsItems = [
 	},
 	{
 		skill_id: 2,
+		skill_name: 'Express',
+		skill_logo: '',
+	},
+	{
+		skill_id: 3,
 		skill_name: 'Mongodb',
 		skill_logo: '',
+	},
+	{
+		skill_id: 4,
+		skill_name: 'PostgreSql',
+		skill_logo: '',
+	},
+
+	{
+		skill_id: 5,
+		skill_name: 'NPM',
+		skill_logo: <RiNpmjsFill />,
+	},
+	{
+		skill_id: 6,
+		skill_name: 'git | github',
+		skill_logo: <RiGithubFill />,
+	},
+	{
+		skill_id: 7,
+		skill_name: 'Javascript',
+		skill_logo: <FaJs />,
+	},
+	{
+		skill_id: 8,
+		skill_name: 'CSS',
+		skill_logo: <FaCss3 />,
+	},
+	{
+		skill_id: 9,
+		skill_name: 'HTML',
+		skill_logo: <RiHtml5Fill />,
 	},
 ];
 
