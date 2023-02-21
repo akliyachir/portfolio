eef835d891d32e927ab0c64042755d9766ca2702
akliyachir <akli.yachir@gmail.com>
2023-02-21 19:13:43 -0300

    Human languages sectio

--- src/ListsAndFixedContent/SpokenHumanLaguages.js
+++ src/ListsAndFixedContent/SpokenHumanLaguages.js
@@ -2,27 +2,27 @@ const SpokenHumanLaguages = [
 	{
 		skill_id: 1,
 		skill_name: 'English',
-		skill_logo: '',
+		skill_logo: 'En',
 	},
 	{
 		skill_id: 2,
 		skill_name: 'French',
-		skill_logo: '',
+		skill_logo: 'Fr',
 	},
 	{
 		skill_id: 3,
 		skill_name: 'Arabic',
-		skill_logo: '',
+		skill_logo: 'Ar',
 	},
 	{
 		skill_id: 4,
 		skill_name: 'Dardja',
-		skill_logo: '',
+		skill_logo: 'Dz',
 	},
 	{
 		skill_id: 5,
 		skill_name: 'Spanish !?',
-		skill_logo: '',
+		skill_logo: 'Es',
 	},
 ];
 
