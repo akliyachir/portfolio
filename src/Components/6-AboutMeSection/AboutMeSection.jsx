98d45a18356240ae4d566e2d6367af69c0d741b9
akliyachir <akli.yachir@gmail.com>
2023-02-21 11:50:35 -0300

    about me section

--- src/Components/6-AboutMeSection/AboutMeSection.jsx
+++ src/Components/6-AboutMeSection/AboutMeSection.jsx
@@ -1,10 +1,13 @@
 import './AboutMeSection.css';
 import aboutMeSectionContent from '../../ListsAndFixedContent/aboutMeSectionContent';
+import '../../img/akli-pic.jpg';
+const profilePicture = '/akli-pic.jpg';
 
 export default function AboutMeSection() {
 	return (
 		<div className='AboutMeSection' id='about_me'>
 			<div className='AboutMeSectionContent'>
+				<div className='profilePicture' />
 				<div className='title'>{aboutMeSectionContent.title}</div>
 				<p className='presentation'>{aboutMeSectionContent.presentation}</p>
 			</div>
