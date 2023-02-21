914862f5112783ed59f228b390100dfb333a019e
akliyachir <akli.yachir@gmail.com>
2023-02-21 13:50:33 -0300

    skills section

--- src/Components/7-SkillsSection/SkillsSection.jsx
+++ src/Components/7-SkillsSection/SkillsSection.jsx
@@ -5,18 +5,21 @@ import { RiReactjsFill } from 'react-icons/ri';
 
 export default function SkillsSection() {
 	return (
-		<div className='SkillsSection' id='skills'>
-			{skillsItems.map((skillsItemsLoop) => {
-				const { skill_id, skill_name, skill_logo } = skillsItemsLoop;
-				console.log(skill_logo);
-				return (
-					<div key={skill_id} className='skillsItemsLoop'>
-						<div className='skill_logo'>{skill_logo}</div>
-						<div>{skill_name}</div>
-					</div>
-				);
-			})}
-			<div></div>
+		<div className='skillSectionContainer'>
+			<div className='skillsSectionTitle'>skills</div>
+			<div className='SkillsSection' id='skills'>
+				{skillsItems.map((skillsItemsLoop) => {
+					const { skill_id, skill_name, skill_logo } = skillsItemsLoop;
+					console.log(skill_logo);
+					return (
+						<div key={skill_id} className='skillsItemsLoop'>
+							<div className='skill_logo'>{skill_logo}</div>
+							<div>{skill_name}</div>
+						</div>
+					);
+				})}
+				<div></div>
+			</div>
 		</div>
 	);
 }
