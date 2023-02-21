3d3a1516bc91a31a68314c968a5cf2d02d7393eb
akliyachir <akli.yachir@gmail.com>
2023-02-21 15:53:59 -0300

    contact section

--- src/ListsAndFixedContent/projectsContent.js
+++ src/ListsAndFixedContent/projectsContent.js
@@ -79,6 +79,86 @@ const projectsContent = [
 			],
 		},
 	},
+	{
+		id: 2,
+		project_name: 'Articles Publishing Platform',
+		project_description:
+			'Mern stack platform to publish articles, with user registration, authentification, authorizations, password encryption, token and local storage, foreign keys, and crud operations.',
+		project_image_preview: '/projectsImages/1-articles-publishing-platform.png',
+		project_link: 'https://my-article-online.netlify.app/',
+		project_github_repository:
+			'https://github.com/akliyachir/Mern_stack_Articles_Publishing_platform',
+		project_tech: {
+			backend: [
+				{
+					tech_id: 1,
+					tech_name: 'express',
+				},
+				{
+					tech_id: 2,
+					tech_name: 'mongodb atlas',
+				},
+				{
+					tech_id: 3,
+					tech_name: 'mongoose',
+				},
+				{
+					tech_id: 4,
+					tech_name: 'jsonwebtoken',
+				},
+				{
+					tech_id: 5,
+					tech_name: 'bcrypt',
+				},
+				{
+					tech_id: 6,
+					tech_name: 'dotenv',
+				},
+				{
+					tech_id: 7,
+					tech_name: 'cors',
+				},
+				{
+					tech_id: 8,
+					tech_name: 'validator',
+				},
+			],
+			frontend: [
+				{
+					tech_id: 1,
+					tech_name: 'vite',
+				},
+				{
+					tech_id: 2,
+					tech_name: 'react 18',
+				},
+				{
+					tech_id: 3,
+					tech_name: 'react router dom 6.8',
+				},
+				{
+					tech_id: 4,
+					tech_name: 'tiptap',
+				},
+				{
+					tech_id: 5,
+					tech_name: 'html-react-parser',
+				},
+				{
+					tech_id: 6,
+					tech_name: 'local storage',
+				},
+				{
+					tech_id: 7,
+					tech_name: 'hooks',
+				},
+				{
+					tech_id: 8,
+					tech_name: 'vanilla css with grid & flexbox',
+				},
+			],
+		},
+	},
 ];
 
 export default projectsContent;
