97915206b5f3009d121e36b2de2b83f3beab2a55
akliyachir <akli.yachir@gmail.com>
2023-02-21 19:06:49 -0300

    Contact section, clickable links

--- src/Components/9-ContactSection/ContactSection.jsx
+++ src/Components/9-ContactSection/ContactSection.jsx
@@ -2,7 +2,7 @@ import './ContactSection.css';
 import ContactInformations from '../../ListsAndFixedContent/ContactInformations';
 import { WindowWidthContext } from '../../contexts/WindowWidth';
 import { useContext } from 'react';
-import { FaLinkedin } from 'react-icons/fa';
+import { FaLinkedin, FaGithub, FaLink } from 'react-icons/fa';
 
 export default function ContactSection() {
 	const pageWidth = useContext(WindowWidthContext);
@@ -17,26 +17,53 @@ export default function ContactSection() {
 	return (
 		<div className='ContactSection' id='contact'>
 			<div className='contactSectionTitle'>Contact</div>
-			<div className='contactContentContainerOfEveryThing'>
-				<div
-					key={contact_id}
-					className={
-						pageWidth > 470 ? 'ContactSectionContent' : 'SmallContactSectionContent'
-					}
-				>
-					<span className='ContactElementName'>Name</span>
-					<span>{contact_full_name} </span>
-					<span className='ContactElementName'>Email</span>
-					<span className='ContactElementInfo'>{contact_email} </span>
-					<span className='ContactElementName'>Phone</span>
-					<span className='ContactElementInfo'>{contact_phone_number} </span>
-					<span className='ContactElementName'>WhatsApp</span>
-					<span className='ContactElementInfo'>{contact_wasap} </span>
-					<span className='ContactElementName'>Adress</span>
-					<span className='ContactElementInfo'>{contact_adress} </span>
-				</div>
-				<div className='ContactClickableLogos'>
-					<FaLinkedin />
+			<div className='firstContactSection'>
+				<div className='contactContentContainerOfEveryThing'>
+					<div
+						key={contact_id}
+						className={
+							pageWidth > 470 ? 'ContactSectionContent' : 'SmallContactSectionContent'
+						}
+					>
+						<span className='ContactElementName'>Name</span>
+						<span>{contact_full_name} </span>
+						<span className='ContactElementName'>Email</span>
+						<span className='ContactElementInfo'>{contact_email} </span>
+						<span className='ContactElementName'>Phone</span>
+						<span className='ContactElementInfo'>{contact_phone_number} </span>
+						<span className='ContactElementName'>WhatsApp</span>
+						<span className='ContactElementInfo'>{contact_wasap} </span>
+						<span className='ContactElementName'>Adress</span>
+						<span className='ContactElementInfo'>{contact_adress} </span>
+					</div>{' '}
+				</div>{' '}
+			</div>
+			<div className='secondContactSection'>
+				<div className='contactContentContainerOfEveryThing'>
+					<div className='ContactClickableLogos'>
+						<a
+							href='http://www.linkedin.com/in/akli-yachir-92b072234'
+							target='_blank'
+							rel='noopener noreferrer'
+						>
+							{' '}
+							<FaLinkedin />
+						</a>
+						<a
+							href='http://github.com/akliyachir'
+							target='_blank'
+							rel='noopener noreferrer'
+						>
+							<FaGithub />
+						</a>
+						<a
+							href='http://akli-yachir.netlify.com'
+							target='_blank'
+							rel='noopener noreferrer'
+						>
+							<FaLink />
+						</a>
+					</div>
 				</div>
 			</div>
 		</div>
